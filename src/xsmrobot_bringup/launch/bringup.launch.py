bdc5f6f314a1b64bb213d0cf402ddc78f1882c07
whilecodespace <2119462912@qq.com>
2026-08-21 21:07:07 +0800

    上位机代码

---
+++ src/xsmrobot_bringup/launch/bringup.launch.py
@@ -0,0 +1,58 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    xsmrobot_bringup_dir = get_package_share_directory(
+        'xsmrobot_bringup')
+    ydlidar_ros2_dir = get_package_share_directory(
+        'ydlidar')
+
+    urdf2tf = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [xsmrobot_bringup_dir, '/launch', '/urdf2tf.launch.py']),
+    )
+
+    odom2tf = launch_ros.actions.Node(
+        package='xsmrobot_bringup',
+        executable='odom2tf',
+        output='screen'
+    )
+
+    microros_agent = launch_ros.actions.Node(
+        package='micro_ros_agent',
+        executable='micro_ros_agent',
+        arguments=['udp4','--port','8888'],
+        output='screen'
+    )
+
+    # 修复1：启动前强制删除残留的虚拟串口文件，避免冲突导致雷达关闭
+    remove_old_tty = launch.actions.ExecuteProcess(
+        cmd=['rm', '-f', '/tmp/tty_laser'],
+        output='screen'
+    )
+
+    ros_serial2wifi =  launch_ros.actions.Node(
+        package='ros_serial2wifi',
+        executable='tcp_server',
+        parameters=[{'serial_port': '/tmp/tty_laser'}],
+        output='screen'
+    )
+
+    ydlidar = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [ydlidar_ros2_dir, '/launch', '/ydlidar_launch.py']),
+    )
+
+    # 使用 TimerAction 启动后 5 秒执行 ydlidar 节点
+    ydlidar_delay = launch.actions.TimerAction(period=5.0, actions=[ydlidar])
+    
+    return launch.LaunchDescription([
+        urdf2tf,
+        odom2tf,
+        microros_agent,
+        #remove_old_tty,   # 清残留
+        ros_serial2wifi,
+        ydlidar_delay
+    ])
